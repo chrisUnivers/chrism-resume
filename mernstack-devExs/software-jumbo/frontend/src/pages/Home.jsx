c6982e7d29b353b28100d2422f2b9372c9d98c4e
ChrisUnivers <chrisonwindow@outlook.com>
2025-04-14 13:31:39 -0700

    initial commit for software-jumbo project.

---
+++ mernstack-devExs/software-jumbo/frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+function Home() {
+
+    return (
+        <>
+            <section className="heading">
+                <h1>Welcome to the world of Software</h1>
+            </section>
+            
+        </>
+    )
+}
+
+export default Home
