35c38d924a18003a09f4b1f73b7e774e60af6b2d
ChrisUnivers <chrisonwindow@outlook.com>
2025-06-18 00:39:09 -0700

    renamed some directores and files due to nextjs naming convention. New home page. Singup singin required only for /artist routes

--- mernstack-devExs/museum-project/frontend/src/app/(notartistpages)/welcome/page.tsx
+++ mernstack-devExs/museum-project/frontend/src/app/(notartistpages)/welcome/page.tsx
@@ -7,18 +7,14 @@ const Welcome = () => {
         <div className="basis-1/2 px-16 mx-auto">
             <h1 className="text-4xl font-bold px-16 mb-4">The World's Art Showcases</h1>
             <p className="text-lg text-gray-400 px-18 mb-8">
-                This is the list of showcase you can see. <br /> Showcase for all artists.
+                This is the list of showcase you can see. Showcase for all artists.
             </p>
             <div className="flex justify-between items-center w-fit px-18">
-                <Link href="/signin">
-                    <div className="pl-16 pr-16 ml-8 mr-16 bg-indigo-400 hover:bg-indigo-300 px-4 py-2 rounded-md">Sign In</div>
-                </Link>
-                <Link href="/signUp">
-                    <div className="pl-16 pr-16 bg-indigo-400 hover:bg-indigo-300 px-4 py-2 rounded-md">Sign Up</div>
+                <Link href="/artist">
+                    <div className="pl-24 pr-24 ml-32 mr-16 bg-indigo-400 hover:bg-indigo-300 px-4 py-2 rounded-md">Artist's Page</div>
                 </Link>
             </div>
         </div>
-
     </div>
   )
 }
