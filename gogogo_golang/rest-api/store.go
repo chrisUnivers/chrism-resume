291f55c91c0442fd50323ae3163ecdcdb081fb7e
ChrisUnivers <chrisonwindow@outlook.com>
2025-10-24 11:42:24 -0700

    implemented createTask and getTask methods of the Store interface

--- gogogo_golang/rest-api/store.go
+++ gogogo_golang/rest-api/store.go
@@ -5,6 +5,7 @@ import "database/sql"
 type Store interface {
 	CreateUser() error
 	CreateTask(t *Task) (*Task, error)
+	GetTask(id string) (*Task, error)
 }
 
 type Storage struct {
@@ -21,6 +22,24 @@ func (s *Storage) CreateUser() error {
 	return nil
 }
 
-// func (s *Storage) CreateTask(t *Task) (*Task, error) {
+func (s *Storage) CreateTask(t *Task) (*Task, error) {
+	rows, err := s.db.Exec("INSERT INTO tasks (name, status, project_id, assigned_to) VALUES (?, ?, ?, ?)", t.Name, t.Status, t.ProjectID, t.AssignedToID)
 
-// }
+	if err != nil {
+		return nil, err
+	}
+
+	id, err := rows.LastInsertId()
+	if err != nil {
+		return nil, err
+	}
+
+	t.ID = id
+	return t, nil
+}
+
+func (s *Storage) GetTask(id string) (*Task, error) {
+	var t Task
+	err := s.db.QueryRow("SELECT id, name, status, project_id, assigned_to, createdAt, FROM tasks WHERE id = ?", id).Scan(&t.ID, &t.Name, &t.Status, &t.ProjectID, &t.AssignedToID, &t.CreatedAt)
+	return &t, err
+}
